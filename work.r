38e6517907fd86f02ecbb7bd18abb083c1e0f628
Gordon <gkamer@outlook.com>
2020-07-04 18:37:52 -0400

    simple work in r

--- work.r
+++ work.r
@@ -1,29 +1,12 @@
-csv1 = read.csv("Batting/00-04.csv")
-csv2 = read.csv("Batting/05-09.csv")
-csv3 = read.csv("Batting/10-14.csv")
-csv4 = read.csv("Batting/15-19.csv")
+total = read.csv("total.csv")
 
-csv5 = read.csv("Pitching/00-04.csv")
-csv6 = read.csv("Pitching/05-09.csv")
-csv7 = read.csv("Pitching/10-14.csv")
-csv8 = read.csv("Pitching/15-19.csv")
+# Wins from WAR sum regression
+mod <- lm(Wtm ~ WARtot, data=total); summary(mod)
+{
+  plot(total$WARtot, total$Wtm, col="black"); 
+  abline(mod, col="red");
+  abline(47, 1, col="blue")
+}
 
-total = rbind(csv1, csv2);
-total <- rbind(total, csv3)
-total <- rbind(total, csv4)
 
-batting <- data.frame("Year"=total$Year, "Tm"=total$Tm, "Wtm"=total$Wtm, "Ltm"=total$Ltm, "tmWL"=total$tmW.L., "G"=total$G, "WARbat"=total$WAR)
 
-head(batting)
-
-total = rbind(csv5, csv6);
-total <- rbind(total, csv7)
-total <- rbind(total, csv8)
-
-pitching <- data.frame("Year"=total$Year, "Tm"=total$Tm, "WARpit"=total$WAR)
-
-total <- merge(batting, pitching, by=c("Year", "Tm"))
-
-total$WARtot <- total$WARbat + total$WARpit
-
-write.csv(total, "total.csv")
