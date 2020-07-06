9de23c6abe0ebf301d345bef7fe95a79653bc0fb
Gordon <gkamer@outlook.com>
2020-07-05 23:14:52 -0400

    prospect develop function draft done

--- mets.py
+++ mets.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 class Team:
 
@@ -41,14 +42,102 @@ class Prospect:
     """
 
     A prospect has, for variables:
-    1. eta - (int) | Estimated time of arrival - year
+    1. eta - (int) | Estimated time of arrival - # of years
     2. fv - (int) | Future value from Fangraphs
-    3. 
+    3. dead - (bool) | Is the player irrecovably injured/otherwise hopeless
+    4. age - (int) | Age of player
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, eta, fv, age):
+        self.eta = eta
+        self.fv = fv
+        self.dead = False
+        self.age = age
+
+    # Develop one year
+    # Process is loosely based on historical data from Fangraphs scouting results
+    def develop(self):
+        self.age += 1  # Prospect ages 1 year
+
+        # Evolve FV
+        # Random walk with probabilities below
+        # -2    -1    0    1    2 
+        # .125 .25  .35  .25 .125
+        fv_draw = random.random()
+        if fv_draw < .125:
+            self.fv = max(self.fv - 10, 20)
+        elif fv_draw < .125 + .25:
+            self.fv = max(self.fv - 5, 20)
+        elif fv_draw < .125 + .25 + .35:
+            pass
+        elif fv_draw < .125 + .25 + .35 + .25:
+            self.fv = min(self.fv + 5, 80)
+        else:
+            self.fv = min(self.fv + 10, 80)
+
+        # Evolve ETA
+        # Markov process with matrix below
+        # 1 is MLB, 7 is Dead
+        """
+            [,1] [,2] [,3] [,4] [,5] [,6] [,7]
+        [1,] 1.00 0.00 0.00 0.00 0.00 0.00  0.0
+        [2,] 0.75 0.15 0.00 0.00 0.00 0.00  0.1
+        [3,] 0.25 0.35 0.30 0.00 0.00 0.00  0.1
+        [4,] 0.05 0.25 0.35 0.25 0.00 0.00  0.1
+        [5,] 0.01 0.05 0.25 0.35 0.24 0.00  0.1
+        [6,] 0.01 0.01 0.05 0.25 0.35 0.23  0.1
+        [7,] 0.00 0.00 0.00 0.00 0.00 0.00  1.0
+
+        """
+        eta_draw = random.random()
+        if self.eta == 1:
+            if eta_draw < .75:
+                self.eta = 0
+            elif eta_draw > .9:
+                self.dead = True
+        elif self.eta == 2:
+            if eta_draw < .25:
+                self.eta = 0
+            elif eta_draw < .25 + .35:
+                self.eta = 1
+            elif eta_draw > .9:
+                self.dead = True
+        elif self.eta == 3:
+            if eta_draw < .05:
+                self.eta = 0
+            elif eta_draw < .05 + .25:
+                self.eta = 1
+            elif eta_draw < .05 + .25 + .35:
+                self.eta = 2
+            elif eta_draw > .9:
+                self.dead = True
+        elif self.eta == 4:
+            if eta_draw < .01:
+                eta_draw = 0
+            elif eta_draw < .01 + .05:
+                self.eta = 1
+            elif eta_draw < .01 + .05 + .25:
+                self.eta = 2
+            elif eta_draw < .01 + .05 + .25 + .35:
+                self.eta = 3
+            elif eta_draw > .9:
+                self.dead = True
+        elif self.eta == 5:
+            if eta_draw < .01:
+                eta_draw = 0
+            elif eta_draw < .01 + .01:
+                eta_draw = 1
+            elif eta_draw < .01 + .01 + .05:
+                self.eta = 2
+            elif eta_draw < .01 + .01 + .05 + .25:
+                self.eta = 3
+            elif eta_draw < .01 + .01 + .05 + .25 + .35:
+                self.eta = 4
+            elif eta_draw > .9:
+                self.dead = True
+
+
 
 # Fangraphs FV to mean WAR
 
--- work.r
+++ work.r
@@ -73,6 +73,45 @@ development_matrix <- matrix(
   ncol=7,
   byrow=TRUE
 )
+t(development_matrix)
 
 library(expm) 
-(development_matrix %^% 4) %*% c(0, 0, 0, 1, 0, 0, 0)
+(development_matrix %^% 2) %*% c(0, 0, 0, 1, 0, 0, 0)
+
+# FV Drift
+
+board_2019 = read.csv("ETA Data/2019-board-data.csv")
+board_2020 = read.csv("ETA Data/2020-board-data.csv")
+
+total_board <- rbind(data.frame("Name"=board_2017$Name), 
+                     data.frame("Name"=board_2018$Name),
+                     data.frame("Name"=board_2019$Name),
+                     data.frame("Name"=board_2020$Name))
+total_board <- unique(total_board)
+
+total_board <- merge(total_board,
+                     data.frame("Name"=board_2017$Name,
+                                "FV2017"=board_2017$FV), by=c("Name"), all=TRUE)
+total_board <- merge(total_board,
+                     data.frame("Name"=board_2018$Name,
+                                "FV2018"=board_2018$FV), by=c("Name"), all=TRUE)
+total_board <- merge(total_board,
+                     data.frame("Name"=board_2019$Name,
+                                "FV2019"=board_2019$FV), by=c("Name"), all=TRUE)
+total_board <- merge(total_board,
+                     data.frame("Name"=board_2020$Name,
+                                "FV2020"=board_2020$FV), by=c("Name"), all=TRUE)
+
+tab1 <- table(total_board$FV2017, total_board$FV2018); tab1
+tab2 <- table(total_board$FV2018, total_board$FV2019); tab2
+tab3 <- table(total_board$FV2019, total_board$FV2020); tab3
+
+prop.table(tab1, 1)
+prop.table(tab2, 1)
+prop.table(tab3, 1)
+
+# Roughly: 
+# Stay probabililty: 30-50
+# Up/down one:       15-35
+# Up/down two:        5-15
+
